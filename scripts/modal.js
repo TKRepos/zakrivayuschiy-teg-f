527c36623cac902546614ec61336531543dd351e
Timur Khamzin <khamzin.timur@gmail.com>
2026-01-21 17:04:11 +0500

    refactor

---
+++ scripts/modal.js
@@ -0,0 +1,6 @@
+const dialog = document.querySelector('.modal');
+const openButton = document.querySelector('.save__button');
+
+openButton.addEventListener('click', () => {
+  dialog.showModal();
+});
